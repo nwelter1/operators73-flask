c582c45ec60672cf7c8ff8014ffae5363d28a38b
nwelter1 <nwelter1@gmail.com>
2021-10-19 13:08:20 -0500

    day-2 morning

--- config.py
+++ config.py
@@ -5,3 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Gives Flask access to relative filepath regardless of OS.
 # Allows outside files/folders to be imported as well
 # can consider this a 'roadmap' we're giving flask for our operating system
+
+class Config:
+    """
+    Sets the configuration variables for our Flask app
+    Eventually we will use hidden variable items, but for now we'll leave them exposed.
+    """
+    SECRET_KEY = "You will never guess..."
--- drone_inventory/__init__.py
+++ drone_inventory/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from config import Config
 from .site.routes import site
+from .authentication.routes import auth
 #instantiating a new flask app
 app = Flask(__name__)
+app.config.from_object(Config)
+
 
 app.register_blueprint(site)
+app.register_blueprint(auth)
 
