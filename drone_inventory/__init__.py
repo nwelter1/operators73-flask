ba103ef03b71c0784a1f70eec27a908b4e017ee6
nwelter1 <nwelter1@gmail.com>
2021-10-19 16:39:32 -0500

    db integration

--- drone_inventory/__init__.py
+++ drone_inventory/__init__.py
@@ -2,11 +2,21 @@ from flask import Flask
 from config import Config
 from .site.routes import site
 from .authentication.routes import auth
+from flask_migrate import Migrate
+from .models import db
+
+
 #instantiating a new flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
+# Registering Blueprints to use within the scope of our whole app
 app.register_blueprint(site)
 app.register_blueprint(auth)
+
+# instantiating db within the scope of our app
+db.init_app(app)
+
+# giving Flask migrate access to app and DB models
+migrate = Migrate(app, db)
 
--- drone_inventory/authentication/routes.py
+++ drone_inventory/authentication/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from drone_inventory.forms import UserLoginForm
+from drone_inventory.models import User, db
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
@@ -11,6 +12,12 @@ def signup():
         email = form.email.data
         password = form.password.data
         print(email, password)
+
+        # create (instantiate) a new user class and add that user into the table
+        new_user = User(email, password)
+        db.session.add(new_user)
+        db.session.commit()
+
     
     return render_template('signup.html', form = form)
 
